ce121fd38931fa3332fd148f7937d0747f837cb3
Luiz Pereira <lfhbpereira@gmail.com>
2023-01-25 21:42:23 -0300

    Add requirement 6 done

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -1,12 +1,9 @@
-// import Fighter from '../Fighter';
+import Fighter from '../Fighter';
 
-// abstract class Battle {
-//   constructor(protected player: Fighter) { }
+export default abstract class Battle {
+  constructor(protected player: Fighter) { }
 
-//   // Should return 1 if player wins, -1 otherwise
-//   fight(): number {
-//     return this.player.lifePoints === -1 ? -1 : 1;
-//   }
-// }
-
-// export default Battle;
+  fight(): number {
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
